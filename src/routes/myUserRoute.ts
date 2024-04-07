084bb0ce7c24fb223eda6b56182a09c11ad8a523
AkshayChandole <akshaychandole1234@gmail.com>
2024-04-07 20:33:22 +0530

    adding user profile feature

--- src/routes/myUserRoute.ts
+++ src/routes/myUserRoute.ts
@@ -1,11 +1,11 @@
-import { validateMyUserRequest } from "../middleware/validation";
 import express from "express";
 import MyUserController from "../controllers/MyUserController";
 import { jwtCheck, jwtParse } from "../middleware/auth";
+import { validateMyUserRequest } from "../middleware/validation";
 
 const router = express.Router();
 
-// Base - /api/my/user
+// /api/my/user
 router.get("/", jwtCheck, jwtParse, MyUserController.getCurrentUser);
 router.post("/", jwtCheck, MyUserController.createCurrentUser);
 router.put(
